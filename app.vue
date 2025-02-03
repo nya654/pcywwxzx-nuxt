6161279059d1b8c7c761f3211737f245ab5d7296
liyang <272891021@qq.com>
2025-02-03 12:51:07 +0800

    chore: update copyright year to 2025
    - Update the copyright year in the footer from 2024 to 2025

--- app.vue
+++ app.vue
@@ -11,7 +11,7 @@ useHead({
       本中心自第16周起暂停预约本学期义务维修工作. 若需紧急协助维修等服务, 请点击下方【QQ咨询群】寻求技术协助!
     </div>
     <NuxtPage class="flex-grow" />
-    <p class="text-xs text-gray-500 text-center p-4 mr-auto">&copy; 2024. 电脑义务维修中心. All rights reserved.</p>
+    <p class="text-xs text-gray-500 text-center p-4 mr-auto">&copy; 2025. 电脑义务维修中心. All rights reserved.</p>
   </div>
 </template>
 
