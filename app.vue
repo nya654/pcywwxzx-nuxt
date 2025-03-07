85326c9174c2e7622760cda1ad8d6ccfeaf9df28
liyang <272891021@qq.com>
2025-03-07 09:09:38 +0800

    refactor(app): 恢复维修功能

--- app.vue
+++ app.vue
@@ -7,9 +7,9 @@ useHead({
 <template>
   <div class="min-h-screen flex flex-col bg-base-100">
     <Header />
-    <div class="alert alert-error alert-soft border-none rounded-none text-center mb-2">
+    <!-- <div class="alert alert-error alert-soft border-none rounded-none text-center mb-2">
       本中心自第16周起暂停预约本学期义务维修工作. 若需紧急协助维修等服务, 请点击下方【QQ咨询群】寻求技术协助!
-    </div>
+    </div> -->
     <NuxtPage class="flex-grow" />
     <p class="text-xs text-gray-500 text-center p-4 mr-auto">&copy; 2025. 电脑义务维修中心. All rights reserved.</p>
   </div>
--- pages/index.vue
+++ pages/index.vue
@@ -19,7 +19,7 @@ dateNum.value = await $fetch('/api/date_num');
           以丰富校园科技文化为己任. 为切实弘扬志愿服务精神, 本中心无偿为全校师生提供电脑义务维修服务.
         </p>
         <div class="flex flex-wrap">
-          <NuxtLink to="/booking" class="btn btn-primary btn-soft mr-4 mb-4 line-through"> 预约维修 </NuxtLink>
+          <NuxtLink to="/booking" class="btn btn-primary btn-soft mr-4 mb-4"> 预约维修 </NuxtLink>
           <div class="join">
             <a href="https://qm.qq.com/q/flBStssyFa" class="btn btn-soft join-item" target="_blank"> QQ咨询群 </a>
             <a href="https://doc.pcywwxzx.top" class="btn btn-soft join-item" target="_blank"> 维修文档 </a>
