14bea63c5d1bd5783713aa20a3402b98a7cf232b
liyang <272891021@qq.com>
2025-03-18 19:16:51 +0800

    sort by app_time

--- server/api/issue_list.js
+++ server/api/issue_list.js
@@ -4,6 +4,6 @@ export default defineEventHandler(async (event) => {
     return '密码错误';
   }
   const db = hubDatabase();
-  const issue_list = (await db.prepare('SELECT * FROM issues ORDER BY reg_time DESC LIMIT 32').all()).results;
+  const issue_list = (await db.prepare('SELECT * FROM issues ORDER BY app_time DESC LIMIT 32').all()).results;
   return issue_list;
 });
